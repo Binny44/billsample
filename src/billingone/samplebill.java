69656969ddb21e865b16db8e7c741308f24aa1c7
BinnyJ <binnyjohn39@gmail.com>
2020-11-01 22:45:27 -0500

    This test one for bill

---
+++ src/billingone/samplebill.java
@@ -0,0 +1,8 @@
+package billingone;
+
+public class samplebill {
+    public static void main(String[] args) {
+        System.out.println("Java is fun");
+        System.out.println("Binny is QA");
+    }
+}
